1c7f562f6c302640bfe0ca92edca7226481455a1
Alex <Alexander3.hillman@live.uwe.ac.uk>
2021-10-15 11:56:06 +0100

    improvements to window handling

--- DirectX11 First Person Shooter/DirectX11 First Person Shooter/WinMain.cpp
+++ DirectX11 First Person Shooter/DirectX11 First Person Shooter/WinMain.cpp
@@ -7,8 +7,52 @@ LPDIRECT3D9 d3d;
 LPDIRECT3DDEVICE9 d3ddev;
 
 void InitD3D(HWND hwnd);
-void Render_frame(void);
+void RenderFrame(void);
 void CleanD3D(void);
+
+void InitD3D(HWND hWnd)
+{
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+	D3DPRESENT_PARAMETERS d3dpp;
+
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow = hWnd;
+
+	d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &d3ddev);
+}
+
+void RenderFrame(void)
+{
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+
+	d3ddev->BeginScene();
+
+	d3ddev->EndScene();
+
+	d3ddev->Present(NULL, NULL, NULL, NULL);
+}
+
+void CleanD3D(void)
+{
+	d3ddev->Release();
+	d3d->Release();
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(1);
+		break;
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
 
 // entry point for the windows program
 int CALLBACK  WinMain(
@@ -17,12 +61,14 @@ int CALLBACK  WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
-	const auto pClassName = L"window";
+	const auto pClassName = L"DirectX";
+	const auto windowName = L"First Person Shooter";
 
+	HWND hWnd;
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -42,13 +88,28 @@ int CALLBACK  WinMain(
 	int width = 640;
 	int height = 480;
 
-	HWND hWnd = CreateWindowEx(
-		0, pClassName, L"window", WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, x_pos, y_pos, width, height, nullptr, nullptr, hInstance, nullptr
+	hWnd = CreateWindowEx(
+		0, pClassName, windowName, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, x_pos, y_pos, width, height, nullptr, nullptr, hInstance, nullptr
 	);
 
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
+	MSG msg;
 
-	return 0;
+	BOOL gResult;
+
+	while((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
