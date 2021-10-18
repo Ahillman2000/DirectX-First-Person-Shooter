421132e4bcad1e733bbe62b06ba17ee0544ede3a
ahillman2000 <alexander.hillman0000@gmail.com>
2021-10-18 12:55:05 +0100

    rendered a traiangle

--- DirectX11 First Person Shooter/DirectX11 First Person Shooter/WinMain.cpp
+++ DirectX11 First Person Shooter/DirectX11 First Person Shooter/WinMain.cpp
@@ -1,46 +1,108 @@
+// include the windows and direct3d header files
 #include<Windows.h>
 #include <d3d9.h>
 
+// include the direct3d library file
 #pragma comment (lib, "d3d9.lib")
 
+// defines the resolution of the screen
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+// pointer to direct3d interface
 LPDIRECT3D9 d3d;
+// pointer to device class
 LPDIRECT3DDEVICE9 d3ddev;
+
+// pointer to vertex buffer
+LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL;
 
 void InitD3D(HWND hwnd);
 void RenderFrame(void);
 void CleanD3D(void);
+void InitGraphics(void);
+
+// create a custom vertex format
+struct CUSTOMVERTEX
+{
+	FLOAT x, y, z, rhw;
+	DWORD colour;
+};
+#define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
 
 void InitD3D(HWND hWnd)
 {
+	// direct3d interface
 	d3d = Direct3DCreate9(D3D_SDK_VERSION);
 
+	// struct for device info
 	D3DPRESENT_PARAMETERS d3dpp;
 
+	// clear struct
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	// windowed program
 	d3dpp.Windowed = TRUE;
+	// discard old frames
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	// window to use direct3d
 	d3dpp.hDeviceWindow = hWnd;
 
 	d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &d3ddev);
+	InitGraphics();
 }
 
 void RenderFrame(void)
 {
-	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+	//DWORD clearColour = D3DCOLOR_XRGB(137, 207, 240);
+	DWORD clearColour = D3DCOLOR_XRGB(0, 0, 0);
+
+	// clear window 
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, clearColour, 1.0f, 0);
 
 	d3ddev->BeginScene();
 
+	// set FVF code with custom struct
+	d3ddev->SetFVF(CUSTOMFVF);
+	// set vertex buffer to draw from
+	d3ddev->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
+	// draw vertices to screen
+	d3ddev->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+
 	d3ddev->EndScene();
 
+	// display frame
 	d3ddev->Present(NULL, NULL, NULL, NULL);
 }
 
 void CleanD3D(void)
 {
+	// close and release to clean
+	v_buffer->Release();
 	d3ddev->Release();
 	d3d->Release();
 }
 
+void InitGraphics(void)
+{
+	CUSTOMVERTEX vertices[] =
+	{
+		// vectices of a triangle from top clockwise
+		{400.0f, 62.5f , 0.5f, 1.0f, D3DCOLOR_XRGB(0, 0, 255)},
+		{650.0f, 500.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(0, 255, 0)},
+		{150.0f, 500.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(255, 0, 0)},
+	};
+
+	// create a vertex buffer
+	d3ddev->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX), 0, CUSTOMFVF, D3DPOOL_MANAGED, &v_buffer, NULL);
+
+	VOID* pVoid;
+	// lock buffer
+	v_buffer->Lock(0, 0, (void**)&pVoid, 0);
+	// copy vertices to vertex buffer
+	memcpy(pVoid, vertices, sizeof(vertices));
+	// release buffer
+	v_buffer->Unlock();
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -89,10 +151,12 @@ int CALLBACK  WinMain(
 	int height = 480;
 
 	hWnd = CreateWindowEx(
-		0, pClassName, windowName, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, x_pos, y_pos, width, height, nullptr, nullptr, hInstance, nullptr
+		0, pClassName, windowName, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, x_pos, y_pos, SCREEN_WIDTH, SCREEN_HEIGHT, nullptr, nullptr, hInstance, nullptr
 	);
 
 	ShowWindow(hWnd, SW_SHOW);
+
+	InitD3D(hWnd);
 
 	MSG msg;
 
@@ -102,7 +166,10 @@ int CALLBACK  WinMain(
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+
+		RenderFrame();
 	}
+	CleanD3D();
 
 	if (gResult == -1)
 	{
